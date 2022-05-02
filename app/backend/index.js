f7010ffe71bbd877eebcd3533a8c60c816911777
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-05-02 00:04:51 -0400

    feat: increment POST function

--- app/backend/index.js
+++ app/backend/index.js
@@ -7,11 +7,16 @@ const PORT = 3001;
 const app = express();
 app.use(bodyParser.json());
 
+const arquivoJson = './questionnaireAnswers.json';
+
 app.post('/collect', (req, res) => {
   const { Pergunta1 } = req.body;
   const { Pergunta2 } = req.body;
   const { Pergunta3 } = req.body;
   const { Pergunta4 } = req.body;
+  let QuantidadePositiva = 0;
+  let QuantidadeNegativa = 0;
+  let QuantidadeNaoAvaliada = 0;
 
   const questionnaireAnswers = {
     Pergunta1: Pergunta1.substring(1,4),
@@ -20,7 +25,20 @@ app.post('/collect', (req, res) => {
     Pergunta4,
   };
 
-  fs.writeFile('./questionnaireAnswers.json', JSON.stringify(questionnaireAnswers))
+  const arrayAnswers = Object.values(questionnaireAnswers);
+
+  arrayAnswers.forEach((answer) => {
+    if (answer === 'Sim') QuantidadePositiva += 1;
+    if (answer === 'Não') QuantidadeNegativa += 1;
+    if (answer === 'Não Sei') QuantidadeNaoAvaliada += 1;
+    if (answer === 'Agora!!') QuantidadePositiva += 2;
+  });
+
+  questionnaireAnswers.QuantidadePositiva = QuantidadePositiva;
+  questionnaireAnswers.QuantidadeNegativa = QuantidadeNegativa;
+  questionnaireAnswers.QuantidadeNaoAvaliada = QuantidadeNaoAvaliada;
+
+  fs.writeFile(arquivoJson, JSON.stringify(questionnaireAnswers))
   .then(() => {
     console.log('Arquivo escrito com sucesso!');
   })
@@ -29,6 +47,18 @@ app.post('/collect', (req, res) => {
   });
 
   res.send(questionnaireAnswers);
+});
+
+app.get('/collect', (_req, res) => {
+  fs.readFile(arquivoJson, 'utf8')
+  .then((data) => {
+    console.log(`Conteúdo do arquivo: ${data}`);
+    res.send(data);
+  })
+  .catch((err) => {
+    console.error(`Não foi possível ler o arquivo ${arquivoJson}\n Erro: ${err}`);
+    process.exit(1); // Encerra a execução do script e informa ao sistema operacional que houve um erro com código
+  });
 });
 
 app.listen(PORT, () => {
